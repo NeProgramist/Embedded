ff46cbff195845d4859853dd152c557a87b0469e
NeProgramist <zaskozhenya@gmail.com>
2021-02-22 16:46:46 +0200

    feat(lab1-1): impl drawer class

---
+++ lab1-1/src/main/kotlin/PlotsDrawer.kt
@@ -0,0 +1,39 @@
+import kscience.plotly.*
+
+class PlotsDrawer {
+    fun createPlot(
+        values: Map<Double, Double>,
+        xaxis: String,
+        yaxis: String
+    ) {
+        val xValues = values.keys
+        val yValues = values.values
+
+        val plot = Plotly.plot {
+            trace {
+                x.set(xValues)
+                y.set(yValues)
+            }
+            layout {
+                width = 1900
+                height = 800
+                margin {
+                    l = 20
+                    t = 20
+                    r = 20
+                    b = 20
+                }
+
+                xaxis {
+                    title = xaxis
+                }
+
+                yaxis {
+                    title = yaxis
+                }
+            }
+        }
+
+        plot.makeFile()
+    }
+}
